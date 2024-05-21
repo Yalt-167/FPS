1adf96d83d2711ea6704d9e8ea9916d3030b070b
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-05-21 20:09:12 +0200

    less expected projectile behaviour

---
+++ Assets/BasicProjectilePierceWall.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicProjectilePierceWall : ProjectileOnHitWallBehaviour
+{
+    public override void OnHitWall(Projectile _, Collider __) { }
+}
