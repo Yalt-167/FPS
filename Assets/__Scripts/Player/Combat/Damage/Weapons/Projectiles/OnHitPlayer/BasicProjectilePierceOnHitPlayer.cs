2917609928b902217125d800039ccfe01a98a2e7
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-05-22 03:54:04 +0200

    basic projectile pierce players

--- Assets/__Scripts/Player/Combat/Damage/Weapons/Projectiles/OnHitPlayer/BasicProjectilePierceOnHitPlayer.cs
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/Projectiles/OnHitPlayer/BasicProjectilePierceOnHitPlayer.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicProjectilePierceOnHitPlayer : MonoBehaviour
+public class BasicProjectilePierceOnHitPlayer : ProjectileOnHitPlayerBehaviour
 {
-    
+    [SerializeField] protected ushort maxPlayerPierceAmount;
+    protected ushort currentPiercedPlayerCount;
+
+    public override void OnHitPlayer(Projectile relevantprojectile, IShootable relevantPlayer)
+    {
+        if (currentPiercedPlayerCount == maxPlayerPierceAmount)
+        {
+            relevantprojectile.Deactivate();
+            return;
+        }
+
+        currentPiercedPlayerCount++;
+    }
 }
