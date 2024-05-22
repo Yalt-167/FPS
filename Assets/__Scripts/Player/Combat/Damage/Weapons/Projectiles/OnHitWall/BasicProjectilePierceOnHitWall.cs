9b78f68957ceb38be49c7b56d48b0fd0ccd239a7
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-05-22 03:54:54 +0200

    fixed logic issue in a previous commit

--- Assets/__Scripts/Player/Combat/Damage/Weapons/Projectiles/OnHitWall/BasicProjectilePierceOnHitWall.cs
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/Projectiles/OnHitWall/BasicProjectilePierceOnHitWall.cs
@@ -11,11 +11,11 @@ public class BasicProjectilePierceOnHitWall : ProjectileOnHitWallBehaviour
     // if several coliders on a singular wall it would count all colliders so keep that in check
     public override void OnHitWall(Projectile relevantProjectile, Collider __)
     {
-        wallPierced++;
-
         if (wallPierced == maxWallAmountToPierce)
         {
             relevantProjectile.Deactivate();
         }
+
+        wallPierced++;
     }
 }
