4290b412115edbfd8654c0ae35a1871ae924ac27
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-05-26 04:58:09 +0200

    scene clean up

--- Assets/__Scripts/Respawn.cs
+++ Assets/__Scripts/Respawn.cs
@@ -10,19 +10,11 @@ public class Respawn : MonoBehaviour
     private bool offLimits;
     void Update()
     {
-        offLimits = Physics.Raycast(transform.position, Vector3.down, 2f * 0.5f + 0.2f, limits);
-        
-        if (offLimits)
-            RespawnPlayer();
-        
-        
+        if (Physics.Raycast(transform.position, Vector3.down, 2f * 0.5f + 0.2f, limits)) { RespawnPlayer(); }     
     }
 
     private void RespawnPlayer()
     {
-
-        transform.position = respawnPoint.position;
-        transform.rotation = respawnPoint.rotation;
-
+        transform.SetPositionAndRotation(respawnPoint.position, respawnPoint.rotation);
     }
 }
