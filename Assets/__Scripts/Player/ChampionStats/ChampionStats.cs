24fed2a90710d7f4fdc7424b55f3b2809c38deac
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-06-08 23:59:06 +0200

    struct
    struct

---
+++ Assets/__Scripts/Player/ChampionStats/ChampionStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChampionStats", menuName = "ScriptableObjects/ChampionStats")]
+public class ChampionStats : ScriptableObject
+{
+    public float RunningSpeed;
+}
