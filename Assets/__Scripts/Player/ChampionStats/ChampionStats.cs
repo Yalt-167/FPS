28d8962253826f5f59b5b37eb8b9fb0275416ddc
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-06-09 05:17:42 +0200

    started taking champions specificities into account

--- Assets/__Scripts/Player/ChampionStats/ChampionStats.cs
+++ Assets/__Scripts/Player/ChampionStats/ChampionStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,49 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ChampionStats", menuName = "ScriptableObjects/ChampionStats")]
 public class ChampionStats : ScriptableObject
 {
-    public float RunningSpeed;
+    public ChampionMovementStats MovementStats;
+
 }
+
+
+[Serializable]
+public struct ChampionMovementStats
+{
+    public ChampionSpeedStats SpeedStats;
+    public ChampionJumpStats JumpStats;
+    public ChampionDashStats DashStats;
+}
+
+[Serializable]
+public struct ChampionSpeedStats
+{
+    public float RunningSpeed;
+    public float StrafingSpeed;
+    public float BackwardSpeed;
+    public float WallRunningSpeed;
+    public float Acceleration;
+}
+
+[Serializable]
+public struct ChampionJumpStats
+{
+    public float JumpForce;
+    public float SideWallJumpForce;
+    public float UpwardWallJumpForce;
+}
+
+
+[Serializable]
+public struct ChampionDashStats
+{
+    public bool HasDash;
+    public float DashVelocity;
+    public float DashDuration;
+}
+
+
+// do a Champion abstract class
+// bind event to action (OnJump, OnDash, OnLand, etc)
+// each champion inherits it
+
+// also account for damage in the stats
