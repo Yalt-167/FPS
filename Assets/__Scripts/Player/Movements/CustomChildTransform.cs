e791e0bdeb9ddde1fb1f19851df2b2c9995900c7
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-07-26 18:33:32 +0200

    transform shenanigans

---
+++ Assets/__Scripts/Player/Movements/CustomChildTransform.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomChildTransform : MonoBehaviour
+{
+    [SerializeField] private Transform parentTransform;
+
+    private MyTransform parentTransformUnupdated;
+
+    [SerializeField] private bool copyPosition;
+    [SerializeField] private bool copyScale;
+    [SerializeField] private bool copyRotation;
+
+
+    private void Start()
+    {
+        parentTransformUnupdated = new MyTransform(parentTransform);
+        print(parentTransformUnupdated.ToString());
+    }
+    private void Update()
+    {
+        if (parentTransform.hasChanged)
+        {
+            if (copyPosition) { CopyPosition(); }
+            if (copyScale) { CopyScale(); }
+            if (copyRotation) { CopyRotation(); }
+        }
+    }
+
+
+    private void LateUpdate()
+    {
+        if (parentTransform.hasChanged)
+        {
+            parentTransform.hasChanged = false;
+        }
+    }
+
+
+    private void CopyPosition()
+    {
+        transform.position += parentTransform.position - parentTransformUnupdated.Position;
+    }
+
+    private void CopyScale()
+    {
+        var scale = parentTransform.localScale;
+        var previousScale = parentTransformUnupdated.Scale;
+
+        var ratios = new Vector3(
+            scale.x / previousScale.x,
+            scale.y / previousScale.y,
+            scale.z / previousScale.z
+            );
+        
+        transform.localScale = transform.localScale.Mask(ratios);
+    }
+
+    private void CopyRotation()
+    {
+        //transform.rotation += parentTransform.rotation - parentTransformUnupdated.Rotation;
+    }
+}
+
+public struct MyTransform
+{
+    public Vector3 Position;
+    public Vector3 Scale;
+    public Quaternion Rotation;
+    
+    public MyTransform(Transform modelTransform)
+    {
+        Position = modelTransform.position;
+        Scale = modelTransform.localScale;
+        Rotation = modelTransform.rotation;
+    }
+
+    public void CopyDataFrom(Transform modelTransform)
+    {
+        Position = modelTransform.position;
+        Scale = modelTransform.localScale;
+        Rotation = modelTransform.rotation;
+    }
+
+    public readonly new string ToString()
+    {
+        return $"MyTransform with position {Position}, scale {Scale}, rotation {Rotation}";
+    }
+}
---
+++ Assets/__Scripts/Player/Movements/PartialUpdateChildTransform.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartialUpdateChildTransform : MonoBehaviour
+{
+
+    private MyTransform previousData;
+    [SerializeField] private bool lockPosition;
+    [SerializeField] private bool lockScale;
+    [SerializeField] private bool lockRotation;
+
+
+    private void Start()
+    {
+        previousData = new MyTransform(transform);
+    }
+
+    private void FixedUpdate()
+    {
+        if (lockPosition)
+        {
+            transform.position = previousData.Position;
+        }
+
+        if (lockScale)
+        {
+            transform.localScale = previousData.Scale;
+        }
+
+        if (lockRotation)
+        {
+            transform.rotation = previousData.Rotation;
+        }
+    }
+}
