596ba438c6b79d7141fbcd0ad3bf932f58a910ab
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-07-27 23:30:52 +0200

    Script to fix gun rotation

---
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/HandleGunAngle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HandleGunAngle : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask shootableLayers;
+    private Transform cameraTransform;
+
+    private void Awake()
+    {
+        cameraTransform = transform.parent;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(GetPointOnCrosshair());
+        print(transform.rotation);
+    }
+
+    private Vector3 GetPointOnCrosshair()
+    {
+        if(Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, float.PositiveInfinity, shootableLayers, QueryTriggerInteraction.Ignore))
+        {
+            return hit.point;
+        }
+        else
+        {
+            return cameraTransform.position + cameraTransform.forward * 1000f;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(cameraTransform.position, cameraTransform.forward * 1000f);
+    }
+}
