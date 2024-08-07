01956dd842bc51e5d490882388df70d32e46bf90
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-08-07 06:54:13 +0200

    new class to handle gameModes names

---
+++ Assets/__Scripts/NetworkGeneralScripts/LobbyHandling/GameModes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameModes
+{
+    public static readonly string TeamMacth = "Teammatch";
+    public static readonly string DeathMatch = "Deathmatch";
+}
