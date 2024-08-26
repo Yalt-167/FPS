e20c5f6323dbf6ee46194e019569155bc0bff6d7
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-08-26 22:07:12 +0200

    more complex structure for maps name handling

--- Assets/__Scripts/NetworkGeneralScripts/LobbyHandling/Maps.cs
+++
@@ -1,7 +0,0 @@
-namespace LobbyHandling
-{
-	public static class Maps // redo that with per mode maps
-	{
-		public static readonly string ToBeVoted = nameof(ToBeVoted);
-	} 
-}
---
+++ Assets/__Scripts/NetworkGeneralScripts/LobbyHandling/Maps/Maps.cs
@@ -0,0 +1,54 @@
+namespace LobbyHandling
+{
+	public static class Maps // redo that with per mode maps
+	{
+		public static readonly string ToBeVoted = nameof(ToBeVoted);
+		public static class TeamFight
+		{
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+
+        public static class DeathMatch
+        {
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+
+        public static class Escort
+        {
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+
+
+        public static class CaptureTheFlag
+        {
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+
+        public static class Hardpoint
+        {
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+
+        public static class Arena
+        {
+            public static readonly string Map1 = nameof(Map1);
+        }
+
+        public static class Touchdown
+        {
+            public static readonly string Map1 = nameof(Map1);
+            public static readonly string Map2 = nameof(Map2);
+            public static readonly string Map3 = nameof(Map3);
+        }
+    } 
+}
