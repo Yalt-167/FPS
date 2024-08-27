376ae8b79e205017db16702b0022afef7a7d20b4
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-08-27 21:40:01 +0200

    modified ability input for it to be more prone to expansion

--- Assets/__Scripts/Player/MyInputs/InputQueries/CombatInputQuery.cs
+++ Assets/__Scripts/Player/MyInputs/InputQueries/CombatInputQuery.cs
@@ -13,13 +13,18 @@ namespace Inputs
         public VariableKeybind Aim = new(KeyCode.Mouse1, new() { PlayerActionActivationType.OnKeyHeld, PlayerActionActivationType.Toggle });
         public FixedKeybind Reload = new(KeyCode.R, PlayerActionActivationType.OnKeyDown);
 
+
         public FixedKeybind FirstGun = new(KeyCode.Alpha1, PlayerActionActivationType.OnKeyDown);
         public FixedKeybind SecondGun = new(KeyCode.Alpha2, PlayerActionActivationType.OnKeyDown);
         public FixedKeybind ThirdGun = new(KeyCode.Alpha3, PlayerActionActivationType.OnKeyDown);
 
-        public FixedKeybind PrimaryAbility;
-        public FixedKeybind SecondaryAbility;
-        public FixedKeybind Ultimate;
+
+        public FixedKeybind InitiatePrimaryAbility;
+        public FixedKeybind ReleasePrimaryAbility;
+        public FixedKeybind InitiateSecondaryAbility;
+        public FixedKeybind ReleaseSecondaryAbility;
+        public FixedKeybind InitiateUltimate;
+        public FixedKeybind ReleaseUltimate;
 
 
         public FixedKeybind Slash;
@@ -40,15 +45,16 @@ namespace Inputs
             SecondGun.Init();
             ThirdGun.Init();
 
-            PrimaryAbility.Init();
-            SecondaryAbility.Init();
-            Ultimate.Init();
+            InitiatePrimaryAbility.Init();
+            ReleasePrimaryAbility.Init();
+            InitiateSecondaryAbility.Init();
+            ReleaseSecondaryAbility.Init();
+            InitiateUltimate.Init();
+            ReleaseUltimate.Init();
 
             Slash.Init();
             Parry.Init();
-
             ChangeCrosshair.Init();
         }
     }
-
 }
