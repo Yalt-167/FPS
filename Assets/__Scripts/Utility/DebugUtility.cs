27f2980a87f3bc55328ff5d24c5a43e0eecd0039
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-03 17:31:56 +0200

    same but after

--- Assets/__Scripts/Utility/DebugUtility.cs
+++ Assets/__Scripts/Utility/DebugUtility.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
+
 using UnityEngine;
+
 using Debug = UnityEngine.Debug;
 
 public static class DebugUtility
@@ -18,11 +21,10 @@ public static class DebugUtility
     {
         var stackTrace = new StackTrace(skipFrames: 1);
 
-        var upperBound = 5 > stackTrace.FrameCount ? stackTrace.FrameCount : 5;
+        var upperBound = 5 > stackTrace.FrameCount ? stackTrace.FrameCount : 5; // basically upperBound = 5;
         for (int i = 0; i < upperBound; i++)
         {
-            StackFrame frame = stackTrace.GetFrame(i);
-            var method = frame.GetMethod();
+            var method = stackTrace.GetFrame(i).GetMethod();
 
             Debug.Log($"[DebugUtility::Log Call Stack Frame {i}]: {method.DeclaringType.FullName}::{method.Name}");
         }
@@ -32,16 +34,16 @@ public static class DebugUtility
     {
         var stringBuilder = new StringBuilder();
 
-        stringBuilder.Append("[ ");
+        _ = stringBuilder.Append("[ ");
 
         var isFirst = true;
         foreach (var item in iterable)
         {
-            stringBuilder.Append(isFirst ? $"{item?.ToString()}" : $", {item?.ToString()}");
+            _ = stringBuilder.Append(isFirst ? $"{item?.ToString()}" : $", {item?.ToString()}");
             isFirst = false;
         }
 
-        stringBuilder.Append(" ]");
+        _ = stringBuilder.Append(" ]");
 
         Debug.Log(stringBuilder.ToString());
     }
