f164809992bf00caae98d21d549d13d7a420920d
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-05 19:43:44 +0200

    Scoreboard

--- Assets/__Scripts/Player/Menus/Scoreboard/Scoreboard.cs
+++ Assets/__Scripts/Player/Menus/Scoreboard/Scoreboard.cs
@@ -2,14 +2,13 @@ using GameManagement;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 namespace Menus
 {
     public sealed class Scoreboard : MonoBehaviour
     {
-        private PlayerScoreboardInfos[] players;
+        private PlayerScoreboardInfos[] playersInfos;
         public static Scoreboard Instance { get; private set; }
 
         private bool doRender;
@@ -27,30 +26,40 @@ namespace Menus
         private void Init()
         {
             Instance = this;
-
-            players = new PlayerScoreboardInfos[GameNetworkManager.Manager.PlayerCount];
+            var simulatedEntries = new string[] { "Diegocardi", "Syn", "Kry", "Asyd"};
+            playersInfos = new PlayerScoreboardInfos[GameNetworkManager.Manager.PlayerCount + simulatedEntries.Length];
 
             for (int index = 0; index < GameNetworkManager.Manager.PlayerCount; index++)
             {
                 ref PlayerFrame playerFrame = ref GameNetworkManager.Manager.Players[index];
                 playerFrame.ScoreboardIndex = index;
-                players[index] = new PlayerScoreboardInfos(playerFrame.TeamNumber, playerFrame.Name.ToString());
+                playersInfos[index] = new PlayerScoreboardInfos(playerFrame.TeamNumber, playerFrame.Name.ToString());
+            }
+
+            CreateFakeEntries(simulatedEntries);
+        }
+
+        private void CreateFakeEntries(string[] entries)
+        {
+            for (int index = 0; index < entries.Length; index++)
+            {
+                playersInfos[GameNetworkManager.Manager.PlayerCount + index] = new PlayerScoreboardInfos(index % 2 + 1, entries[index]);
             }
         }
 
         public void AddKill(int tryharderIndex)
         {
-            players[tryharderIndex].Kills++;
+            playersInfos[tryharderIndex].Kills++;
         }
 
         public void AddDeath(int deadGuyIndex)
         {
-            players[deadGuyIndex].Deaths++;
+            playersInfos[deadGuyIndex].Deaths++;
         }
 
         public void AddAssist(int KSedIndividual)
         {
-            players[KSedIndividual].Deaths++;
+            playersInfos[KSedIndividual].Deaths++;
         }
 
         public void SetDoRender(bool doRender_)
@@ -62,11 +71,51 @@ namespace Menus
         {
             if (!doRender) { return; }
 
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginVertical(CachedGUIStylesNames.Box, GUILayout.Width(600));
+
+            DrawScoreboardHeader();
+
+            foreach (var playerInfos in playersInfos)
+            {
+                DrawPlayerSection(playerInfos);
+            }
+
+            GUILayout.EndVertical();
+
+            GUILayout.FlexibleSpace();
+
+            GUILayout.EndHorizontal();
+        }
+
+        private void DrawScoreboardHeader()
+        {
+            GUI.color = Color.black;
+            
             GUILayout.BeginHorizontal(CachedGUIStylesNames.Box);
 
-            GUILayout.Label("W content");
+            GUILayout.Label("Players", GUILayout.Width(200));
+            GUILayout.Label("Champions", GUILayout.Width(200));
+            GUILayout.Label("K/D/A", GUILayout.Width(200));
+
+            GUILayout.EndHorizontal();
+
+            GUI.color = Color.white;
+        }
+
+        private void DrawPlayerSection(PlayerScoreboardInfos playerInfos)
+        {
+            GUILayout.BeginHorizontal(CachedGUIStylesNames.Box);
+
+            GUILayout.Label(playerInfos.Name, GUILayout.Width(200));
+            GUILayout.Label(playerInfos.CurrentChampion, GUILayout.Width(200));
+            GUILayout.Label($"{playerInfos.Kills}/{playerInfos.Deaths}/{playerInfos.Assists}", GUILayout.Width(200));
 
             GUILayout.EndHorizontal();
         }
     }
 }
+// so far when the Gravity relies on PlayerMovement which is deactivated when a menu is opened
