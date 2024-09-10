1920275427ed3d0772634de5974e92223409919d
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-10 19:00:15 +0200

    methods overloads for scene loading

--- Assets/__Scripts/Environment/SceneHandling/SceneLoader.cs
+++ Assets/__Scripts/Environment/SceneHandling/SceneLoader.cs
@@ -12,6 +12,7 @@ namespace SceneHandling
         public static SceneLoader Instance;
 
         private readonly List<string> loadedScenes = new();
+        private readonly List<int> loadedScenesIndexes = new();
 
         private void Awake()
         {
@@ -20,7 +21,8 @@ namespace SceneHandling
 
         private void Start()
         {
-            LoadSceneAsync("_Scenes/DebugOverlay", true);
+            //LoadSceneAsync("_Scenes/DebugOverlay", true);
+            LoadSceneAsync(2, true);
         }
 
         public  void LoadSceneAsync(string scene, bool additive)
@@ -30,6 +32,14 @@ namespace SceneHandling
             if (loadedScenes.Contains(scene)) { return; }
 
             StartCoroutine(LoadSceneAsyncInternal(scene, additive));
+        }
+        public void LoadSceneAsync(int sceneIndex, bool additive)
+        {
+            if (SceneMissing(sceneIndex)) { return; }
+
+            if (loadedScenesIndexes.Contains(sceneIndex)) { return; }
+
+            StartCoroutine(LoadSceneAsyncInternal(sceneIndex, additive));
         }
 
         private IEnumerator LoadSceneAsyncInternal(string scene, bool additive)
@@ -42,6 +52,17 @@ namespace SceneHandling
             }
 
             loadedScenes.Add(scene);
+        }
+        private IEnumerator LoadSceneAsyncInternal(int sceneIndex, bool additive)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex, additive ? LoadSceneMode.Additive : LoadSceneMode.Single);
+
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+
+            loadedScenesIndexes.Add(sceneIndex);
         }
 
         public void UnloadSceneAsync(string scene)
@@ -72,6 +93,20 @@ namespace SceneHandling
             else
             {
                 Debug.Log($"Tried loading the scene: {scene} however it doesn t exist or isn t registered");
+                return true;
+            }
+        }
+        private bool SceneMissing(int sceneIndex)
+        {
+            var scene = SceneManager.GetSceneByBuildIndex(sceneIndex);
+            if (scene.IsValid())
+            {
+                Debug.Log(scene.path);
+                return false;
+            }
+            else
+            {
+                Debug.Log($"Tried loading the scene: {sceneIndex} however it doesn t exist or isn t registered");
                 return true;
             }
         }
