4e261e4cde2af05e7c4c46d7037874a7e219f10d
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-10 23:54:30 +0200

    moved this

--- Assets/DeprecatedMenu/OptionsMenu.cs
+++ Assets/DeprecatedMenu/OptionsMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
+public sealed class OptionsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
---
+++ Assets/ImportedScripts/PlayAnimOnKeyUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimOnKeyUp : MonoBehaviour {
+
+    public GameObject mainProjectile;
+    public ParticleSystem mainParticleSystem;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            mainProjectile.SetActive(true);
+        }
+
+        mainProjectile.SetActive(mainParticleSystem.IsAlive());
+	}
+}
