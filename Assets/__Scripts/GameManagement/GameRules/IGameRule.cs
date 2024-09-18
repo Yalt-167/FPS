b18c877c4f8889d2bfa1a3a575da52e026a84e37
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-18 22:08:15 +0200

    gamerule became an interface

---
+++ Assets/__Scripts/GameManagement/GameRules/IGameRule.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GameManagement
+{
+    public interface IGameRule
+    {
+        public IGameRuleState GameRuleState { get; }
+
+        #region Game Start Handling
+
+        public event Action OnGameStartedServerSide;
+        public event Action OnGameStartedClientSide;
+
+        public void StartGameServerSide();
+
+        public void StartGameClientSide();
+
+        #endregion
+
+        #region Game Update Handling
+
+        public event Action OnGameUpdatedServerSide;
+        public event Action<IClientSideGameRuleUpdateParam> OnGameUpdatedClientSide;
+
+        public IClientSideGameRuleUpdateParam UpdateGameServerSide();
+
+        public WinInfos UpdateGameClientSide(IClientSideGameRuleUpdateParam param);
+
+        #endregion
+
+        #region Game End Handling
+
+        public event Action OnGameEndedServerSide;
+        public event Action OnGameEndedClientSide;
+
+        public void EndGameServerSide(WinInfos winInfos);
+
+        public void EndGameClientSide(WinInfos winInfos);
+
+        #endregion
+    }
+}
