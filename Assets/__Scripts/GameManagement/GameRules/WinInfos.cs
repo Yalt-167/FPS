c0e19abaa5f56e477c5edb342696c8cdc9814ed6
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-21 18:08:41 +0200

    struct to pack the win infos

--- Assets/__Scripts/GameManagement/GameRules/WinInfos.cs
+++ Assets/__Scripts/GameManagement/GameRules/WinInfos.cs
@@ -1,13 +1,20 @@
+using Unity.Netcode;
+
 namespace GameManagement
 {
     [System.Serializable]
-    public struct WinInfos
+    public struct WinInfos : INetworkSerializable
     {
         public int WinningTeamNumber; // 0 if none yet
 
         public WinInfos(int teamNumber)
         {
             WinningTeamNumber = teamNumber;
+        }
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref WinningTeamNumber);
         }
     }
 }
