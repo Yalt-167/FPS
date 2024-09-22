d5c2071d9c14cbf6795429c8f2d3e8d16af0afd1
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-22 21:21:19 +0200

    class to handle all the scene names

---
+++ Assets/__Scripts/Environment/SceneHandling/Scenes.cs
@@ -0,0 +1,27 @@
+#define DEV_BUILD
+
+namespace SceneHandling
+{
+    public static class Scenes
+    {
+        public static string GetSceneFromGamemodeAndMap(string gamemode, string map)
+        {
+            return $"_Scenes/{gamemode}/{map}";
+        }
+
+        public static readonly string LoginScene = $"_Scenes/{nameof(LoginScene)}";
+
+
+        public static class HUD
+        {
+    #if DEV_BUILD
+            public static readonly string DebugOverlay = $"_Scenes/HUD/{nameof(DebugOverlay)}";
+    #endif
+
+            public static readonly string MainScoreboardHUD = $"_Scenes/HUD/{nameof(MainScoreboardHUD)}";
+        }
+
+    }
+
+
+}
