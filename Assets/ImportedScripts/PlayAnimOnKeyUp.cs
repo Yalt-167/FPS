1a2cdffd8e49a83bb4b3d510082839627d261c84
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-09-25 23:53:55 +0200

    still don t know why its there but made the code pretty

--- Assets/ImportedScripts/PlayAnimOnKeyUp.cs
+++ Assets/ImportedScripts/PlayAnimOnKeyUp.cs
@@ -1,19 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayAnimOnKeyUp : MonoBehaviour {
 
     public GameObject mainProjectile;
     public ParticleSystem mainParticleSystem;
 
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            mainProjectile.SetActive(true);
-        }
-
-        mainProjectile.SetActive(mainParticleSystem.IsAlive());
+	void Update ()
+    {
+        mainProjectile.SetActive(mainParticleSystem.IsAlive() && Input.GetKeyUp(KeyCode.Space));
 	}
 }
