411a4c101de0d0c5684f97e8877baa8bd0e783e5
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-02 20:11:23 +0200

    handling of said If attr

---
+++ Assets/__Scripts/Utility/EditorUtilities/SerializeFieldIfDrawer.cs
@@ -0,0 +1,42 @@
+
+
+using UnityEditor;
+using UnityEngine;
+
+namespace MyEditorUtilities
+{
+    [CustomPropertyDrawer(typeof(IfAttribute))]
+    public class SerializeFieldIfDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!ShouldBeSerialized(property)) { return; }
+
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+        /// <summary>
+        /// Necessary when there are other fields to serialize because otherwise it would skip the space need for the serializeation of this variable before serializing the next field even when this one is not serialized (this probably makes no sense but idc im solo so far
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return ShouldBeSerialized(property) ? EditorGUI.GetPropertyHeight(property, label) : 0f; // doesn t account for the padding between each variable
+        }
+
+        private bool ShouldBeSerialized(SerializedProperty property)
+        {
+            IfAttribute ifAttribute = (IfAttribute)attribute; 
+
+            SerializedProperty conditionField = property.serializedObject.FindProperty(ifAttribute.ConditionField);
+
+            _ = conditionField.propertyType == SerializedPropertyType.Boolean ? (object)null : throw new System.Exception("The condition field is not a boolean");
+
+            return ifAttribute.InvertCondition ? !conditionField.boolValue : conditionField.boolValue;
+        }
+    }
+}
+
+
+
