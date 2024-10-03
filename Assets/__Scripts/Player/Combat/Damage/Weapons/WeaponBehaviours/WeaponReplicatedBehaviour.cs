2ffce036eca4e862e0c7db9dd8656715faa85d2d
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-03 22:53:39 +0200

    weapon replicated behaviour base class

---
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponReplicatedBehaviour.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.Netcode;
+
+using UnityEngine;
+
+namespace WeaponHandling
+{
+    public abstract class WeaponReplicatedBehaviour : NetworkBehaviour
+    {
+
+    }
+}
