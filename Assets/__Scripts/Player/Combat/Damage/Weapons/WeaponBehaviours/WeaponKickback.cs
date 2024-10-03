94c92e0aeffc6001b179a3a754145067045824b3
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-03 22:51:49 +0200

    weapon kickback class

---
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponKickback.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using MyEditorUtilities;
+
+namespace WeaponHandling
+{
+    public sealed class WeaponKickback : WeaponBehaviour
+    {
+        [SerializeField] private Transform weaponTransform; // not sustainable for dual wielding weapons
+        private KickbackStats kickbackStats => weaponHandler.CurrentWeapon.KickbackStats;
+        private WeaponHandler weaponHandler;
+
+        private void Awake()
+        {
+            Setup();
+            weaponHandler = GetComponent<WeaponHandler>();
+            weaponTransform = transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0);
+        }
+
+        private void ApplyKickback(float chargeRatio = 1f)
+        {
+            weaponTransform.localPosition -= new Vector3(0f, 0f, kickbackStats.WeaponKickBackPerShot * chargeRatio);
+        }
+
+        private void HandleKickback()
+        {
+            weaponTransform.localPosition = Vector3.Slerp(weaponTransform.localPosition, Vector3.zero, kickbackStats.WeaponKickBackRegulationTime * Time.time);
+        }
+
+        private void Update()
+        {
+            HandleKickback();
+        }
+    }
+}
