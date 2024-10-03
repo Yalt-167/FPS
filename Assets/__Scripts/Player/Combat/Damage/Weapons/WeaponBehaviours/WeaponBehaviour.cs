364accae2217e874c2b28856ebc3a0bfda9cb219
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-03 22:51:39 +0200

    weaponBehaviour base class

---
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponBehaviour.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace WeaponHandling
+{
+    public abstract class WeaponBehaviour : MonoBehaviour
+    {
+        // public abstract void InitWeapon();
+
+        // public abstract void SwitchToWeapon();
+
+        // public abstract void SwitchOffWeapon();
+
+        protected void Setup()
+        {
+
+        }
+    }
+}
