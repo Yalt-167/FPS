f5bb2bea59bcebb8f85cd22bd70c57c95cac17ad
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-13 21:49:05 +0200

    removed some mess

--- Assets/__Scripts/Utility/EditorUtilities/ConditionalSerialization/ConditionalSerializationAttribute.cs
+++ Assets/__Scripts/Utility/EditorUtilities/ConditionalSerialization/ConditionalSerializationAttribute.cs
@@ -12,24 +12,4 @@ namespace MyEditorUtilities
         public string ConditionField { get; protected set; }
         public bool InvertCondition { get; protected set; }
     }
-
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-    public class IfAttribute : ConditionalSerializationAttribute
-    {
-        public IfAttribute(string conditionField, bool invertCondition = false)
-        {
-            ConditionField = conditionField;
-            InvertCondition = invertCondition;
-        }
-    }
-
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-    public class SerializeFieldIfAttribute : ConditionalSerializationAttribute
-    {
-        public SerializeFieldIfAttribute(string conditionField, bool invertCondition = false)
-        {
-            ConditionField = conditionField;
-            InvertCondition = invertCondition;
-        }
-    }
 }
