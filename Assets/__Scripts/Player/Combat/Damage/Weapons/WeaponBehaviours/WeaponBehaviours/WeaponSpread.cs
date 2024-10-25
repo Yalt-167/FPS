b4f1d6154f46519717c1574b273cd0c524578470
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-25 23:56:50 +0200

    remade the WeaponSpread.cs netcode so it actually works (yay)

--- Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponBehaviours/WeaponSpread.cs
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponBehaviours/WeaponSpread.cs
@@ -16,12 +16,15 @@ namespace WeaponHandling
 
         private NetworkVariable<float> currentSpreadAngle = new NetworkVariable<float>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
         private NetworkVariable<Vector3> directionWithSpread = new NetworkVariable<Vector3>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
-
+        public Vector3 GetDirectionWithSpread(int barrelEndIndex)
+        {
+            SetDirectionWithSpreadServerRpc(barrelEndIndex);
+            return directionWithSpread.Value;
+        }
         // as networkVar to prevent tampering with those
         private NetworkVariable<SimpleShotStats> aimingSimpleShotStats = new NetworkVariable<SimpleShotStats>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
         private NetworkVariable<SimpleShotStats> hipfireSimpleShotStats = new NetworkVariable<SimpleShotStats>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
 
-        private Transform barrelEnd;
         private WeaponHandler weaponHandler;
         private bool IsAiming => weaponHandler.IsAiming;
 
@@ -60,16 +63,18 @@ namespace WeaponHandling
 
 
         [Rpc(SendTo.Server)]
-        public void SetDirectionWithSpreadServerRpc()
+        public void SetDirectionWithSpreadServerRpc(int barrelEndIndex)
         {
-            var spreadStrength = currentSpreadAngle.Value / 45f;
-            /*Most fucked explanantion to ever cross the realm of reality
+            var barrelEnd = weaponHandler.BarrelEnds[barrelEndIndex].transform;
+            /*
+             Most fucked explanantion to ever cross the realm of reality
              / 45f -> to get value which we can use in a vector instead of an angle
             ex in 2D:  a vector that has a 45 angle above X has a (1, 1) direction
             while the X has a (1, 0)
             so we essentially brought the 45 to a value we could use as a direction in the vector
              */
-            // perhaps do directionTransform.forward * 45 instead of other / 45 (for performances purposes)
+
+            var spreadStrength = currentSpreadAngle.Value / 45f;
             directionWithSpread.Value = (
                 barrelEnd.forward + barrelEnd.TransformDirection(
                     new Vector3(
@@ -81,16 +86,16 @@ namespace WeaponHandling
             ).normalized;
 
 
-            // should be the same unless im retarded (no way huh? ^^)
-            directionWithSpread.Value = (
-                barrelEnd.forward + barrelEnd.TransformDirection(
-                    new Vector3(
-                        UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
-                        UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
-                        45f
-                    )
-                )
-            ).normalized;
+            //// should be the same unless im retarded (no way huh? ^^)
+            //directionWithSpread.Value = (
+            //    barrelEnd.forward + barrelEnd.TransformDirection(
+            //        new Vector3(
+            //            UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
+            //            UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
+            //            45f
+            //        )
+            //    )
+            //).normalized;
         }
     }
 }
