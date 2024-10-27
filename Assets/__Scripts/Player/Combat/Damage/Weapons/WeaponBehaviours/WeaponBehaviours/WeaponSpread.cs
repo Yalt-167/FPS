0f8db4d2d8816a754c22bde96cc73ecbef1122a3
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-10-27 21:40:31 +0100

    NETWORK_LIST_AMALGAMATION + methods version for when already in a ServerRpc

--- Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponBehaviours/WeaponSpread.cs
+++ Assets/__Scripts/Player/Combat/Damage/Weapons/WeaponBehaviours/WeaponBehaviours/WeaponSpread.cs
@@ -1,3 +1,5 @@
+#define NETWORK_LIST_AMALGAMATION
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,24 +15,41 @@ namespace WeaponHandling
     /// </summary>
     public sealed class WeaponSpread : NetworkBehaviour // is only requested clientside so no need to be replicated
     {
-        [SerializeField] private bool isShotgun;
-
         private NetworkVariable<float> currentSpreadAngle = new NetworkVariable<float>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
         private NetworkVariable<Vector3> directionWithSpread = new NetworkVariable<Vector3>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
+#if NETWORK_LIST_AMALGAMATION
+        private NetworkVariable<List<Vector3>> shotgunDirectionsWithSpread = new NetworkVariable<List<Vector3>>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
+#else
         private NetworkList<Vector3> shotgunDirectionsWithSpread = new NetworkList<Vector3>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
+#endif
+        // make that a List<NetworkVariable<Vector3>> XD
         public Vector3 GetDirectionWithSpread(int barrelEndIndex)
         {
             SetDirectionWithSpreadServerRpc(barrelEndIndex);
             return directionWithSpread.Value;
         }
 
+        public Vector3 GetDirectionWithSpreadFromServer(int barrelEndIndex)
+        {
+            SetDirectionWithSpreadFromServer(barrelEndIndex);
+            return directionWithSpread.Value;
+        }
+
         public IEnumerable<Vector3> GetShotgunDirectionsWithSpread(int barrelEndIndex)
         {
             SetShotgunDirectionsWithSpreadServerRpc(barrelEndIndex);
+#if NETWORK_LIST_AMALGAMATION
+            var count = shotgunDirectionsWithSpread.Value.Count;
+#else
             var count = shotgunDirectionsWithSpread.Count;
+#endif
             for (int i = 0; i < count; i++)
             {
+#if NETWORK_LIST_AMALGAMATION
+                yield return shotgunDirectionsWithSpread.Value[i];
+#else
                 yield return shotgunDirectionsWithSpread[i];
+#endif
             }
         }
 
@@ -66,10 +85,15 @@ namespace WeaponHandling
             currentSpreadAngle.Value += (IsAiming ? aimingSimpleShotStats.Value.SpreadAngleAddedPerShot : hipfireSimpleShotStats.Value.SpreadAngleAddedPerShot) * chargeRatio;
         }
 
+        public void ApplySpreadFromServer(float chargeRatio)
+        {
+            currentSpreadAngle.Value += (IsAiming ? aimingSimpleShotStats.Value.SpreadAngleAddedPerShot : hipfireSimpleShotStats.Value.SpreadAngleAddedPerShot) * chargeRatio;
+        }
+
         [Rpc(SendTo.Server)]
         public void HandleSpreadServerRpc()
         {
-            currentSpreadAngle.Value = Mathf.Lerp(currentSpreadAngle.Value, 0f, (IsAiming ? aimingSimpleShotStats.Value.SpreadRegulationSpeed : hipfireSimpleShotStats.Value.SpreadRegulationSpeed) * Time.time);
+            currentSpreadAngle.Value = Mathf.Lerp(currentSpreadAngle.Value, 0f, (IsAiming ? aimingSimpleShotStats.Value.SpreadRegulationSpeed : hipfireSimpleShotStats.Value.SpreadRegulationSpeed) * Time.deltaTime);
         }
 
 
@@ -125,12 +149,51 @@ namespace WeaponHandling
         }
 
 
+        private void SetDirectionWithSpreadFromServer(int barrelEndIndex)
+        {
+            /*
+             Most fucked explanantion to ever cross the realm of reality
+             / 45f -> to get value which we can use in a vector instead of an angle
+            ex in 2D:  a vector that has a 45 angle above X has a (1, 1) direction
+            while the X has a (1, 0)
+            so we essentially brought the 45 to a value we could use as a direction in the vector
+             */
+            var spreadStrength = currentSpreadAngle.Value / 45f;
+            directionWithSpread.Value = GetDirectionWithSpreadInternal(spreadStrength, weaponHandler.BarrelEnds[barrelEndIndex].transform);
+
+            //    (
+            //    barrelEnd.forward + barrelEnd.TransformDirection(
+            //        new Vector3(
+            //            UnityEngine.Random.Range(-spreadStrength, spreadStrength),
+            //            UnityEngine.Random.Range(-spreadStrength, spreadStrength),
+            //            0
+            //        )
+            //    )
+            //).normalized;
+
+
+            //// should be the same unless im retarded (no way huh? ^^)
+            //directionWithSpread.Value = (
+            //    barrelEnd.forward + barrelEnd.TransformDirection(
+            //        new Vector3(
+            //            UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
+            //            UnityEngine.Random.Range(-currentSpreadAngle.Value, currentSpreadAngle.Value),
+            //            45f
+            //        )
+            //    )
+            //).normalized;
+        }
+
         [Rpc(SendTo.Server)]
         private void SetShotgunDirectionsWithSpreadServerRpc(int barrelEndIndex)
         {
             var barrelEnd = weaponHandler.BarrelEnds[barrelEndIndex].transform;
 
+#if NETWORK_LIST_AMALGAMATION
+            shotgunDirectionsWithSpread.Value.Clear();
+#else
             shotgunDirectionsWithSpread.Clear();
+#endif
 
             var relevantSpread = IsAiming ? weaponHandler.CurrentWeapon.ShotgunStats.AimingPelletsSpreadAngle : weaponHandler.CurrentWeapon.ShotgunStats.PelletsSpreadAngle;
             relevantSpread /= 45f;
@@ -138,7 +201,11 @@ namespace WeaponHandling
             var pelletCount = weaponHandler.CurrentWeapon.ShotgunStats.PelletsCount;
             for (int i = 0; i < pelletCount; i++)
             {
+#if NETWORK_LIST_AMALGAMATION
+                shotgunDirectionsWithSpread.Value.Add(GetDirectionWithSpreadInternal(relevantSpread, barrelEnd));
+#else
                 shotgunDirectionsWithSpread.Add(GetDirectionWithSpreadInternal(relevantSpread, barrelEnd));
+#endif
             }
         }
 
